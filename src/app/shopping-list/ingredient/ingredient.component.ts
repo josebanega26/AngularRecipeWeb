83e7b3c234d775799e7e01130aa58773fa110148
Jose Banega <banegajose26@gmail.com>
2020-03-31 18:04:01 -0300

    Styling Recipe Detail

--- src/app/shopping-list/ingredient/ingredient.component.ts
+++ src/app/shopping-list/ingredient/ingredient.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model'
 @Component({
   selector: 'app-ingredient',
   templateUrl: './ingredient.component.html',
   styleUrls: ['./ingredient.component.scss']
 })
 export class IngredientComponent implements OnInit {
-
+@Input() ingredient: Ingredient;
   constructor() { }
-
   ngOnInit(): void {
   }
 

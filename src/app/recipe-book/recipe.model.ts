7d8c9ddf348c990db7b2080bfef37281e04b90d1
Jose Banega <banegajose26@gmail.com>
2020-03-31 17:07:40 -0300

    Add ingredients to recipe Model

--- src/app/recipe-book/recipe.model.ts
+++ src/app/recipe-book/recipe.model.ts
@@ -1,12 +1,18 @@
+import {Ingredient} from '../shared/ingredient.model'
 export class Recipe {
     public title: string;
     public description: string;
     public imageUrl: string;
-
-    constructor(title: string, description: string, imageUrl: string) {
+    public ingredients : Ingredient[];
+    constructor(
+        title: string,
+        description: string,
+        imageUrl: string,
+        ingredients : Ingredient[]) {
         this.title = title;
         this.description = description;
-        this.imageUrl = imageUrl
+        this.imageUrl = imageUrl;
+        this.ingredients = ingredients;
     }
 
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,19 +1,29 @@
-import { EventEmitter } from '@angular/core';
-import {Recipe} from '../recipe-book/recipe.model'
-
+import { EventEmitter } from "@angular/core";
+import { Recipe } from "../recipe-book/recipe.model";
+import {Ingredient} from '../shared/ingredient.model'
 export class RecipeService {
-onRecipeSelected= new EventEmitter<Recipe>();    
+  onRecipeSelected = new EventEmitter<Recipe>();
 
-recipes: Recipe[] = [ 
-    new Recipe('Papa Brava',
-                'Locas Papas bravas',
-                'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg'),
-    new Recipe('cheesecake ',
-                'the chocolate cheesecake',
-                'https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2018/06/receta-super-facilisima-de-cheesecake-sin-horno.jpg'),
-
-              ]
-
-    
-
+  recipes: Recipe[] = [
+    new Recipe(
+      "Papa Brava",
+      "Locas Papas bravas",
+      "https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg",
+      [
+        new Ingredient("Papas", 5),
+        new Ingredient("Aji", 2),
+        new Ingredient("Lemon", 2)
+      ]
+    ),
+    new Recipe(
+      "cheesecake ",
+      "the chocolate cheesecake",
+      "https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2018/06/receta-super-facilisima-de-cheesecake-sin-horno.jpg",
+    [
+        new Ingredient("cheese", 2),
+        new Ingredient("chocolate", 5),
+        new Ingredient("Harina", 2)
+      ]
+    )
+  ];
 }

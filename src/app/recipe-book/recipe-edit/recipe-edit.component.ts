46218a930f5f5396fdd33c95e12be8010f4c7bf8
Jose Banega <banegajose26@gmail.com>
2020-04-09 20:02:40 -0300

    Add validation to the Recipe Form

--- src/app/recipe-book/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
 import { RecipeService } from "../../services/recipe.service";
 import { Recipe } from "../recipe.model";
-import { FormGroup, FormControl, FormArray } from "@angular/forms";
+import { FormGroup, FormControl, FormArray, Validators } from "@angular/forms";
 @Component({
   selector: "app-recipe-edit",
   templateUrl: "./recipe-edit.component.html",
@@ -30,6 +30,17 @@ export class RecipeEditComponent implements OnInit {
     return (<FormArray>this.recipeForm.get("ingredients")).controls;
   }
 
+  addIngredient() {
+    (<FormArray>this.recipeForm.get("ingredients")).push(
+      new FormGroup({
+        name: new FormControl(null,Validators.required),
+        amount: new FormControl(null, [
+              Validators.required,
+              Validators.pattern(/^[1-9]+[0-9]*$/),
+        ]),
+      })
+    );
+  }
   onSubmit() {
     console.log("this.recipeForm", this.recipeForm);
   }
@@ -46,16 +57,22 @@ export class RecipeEditComponent implements OnInit {
       for (let index = 0; index < recipe.ingredients.length; index++) {
         formIngredients.push(
           new FormGroup({
-            name: new FormControl(recipe.ingredients[index].name),
-            amount: new FormControl(recipe.ingredients[index].amount),
+            name: new FormControl(
+              recipe.ingredients[index].name,
+              Validators.required
+            ),
+            amount: new FormControl(recipe.ingredients[index].amount, [
+              Validators.required,
+              Validators.pattern(/^[1-9]+[0-9]*$/),
+            ]),
           })
         );
       }
     }
     this.recipeForm = new FormGroup({
-      title: new FormControl(formName),
-      imageUrl: new FormControl(formImageUrl),
-      description: new FormControl(formDescription),
+      title: new FormControl(formName, Validators.required),
+      imageUrl: new FormControl(formImageUrl, Validators.required),
+      description: new FormControl(formDescription, Validators.required),
       ingredients: formIngredients,
     });
   }
